5323ab2e1207d32251e86fdcb27f989ee834402d
Ogurtsov Andrei <ogurcovwm@gmail.com>
2024-03-17 16:28:16 +0200

    Create some stuff

--- src/ui/hud/mod.rs
+++ src/ui/hud/mod.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 
+use crate::game_state::UpdateUI;
 use crate::ui::hud::systems::layout::spawn_hud;
 use crate::ui::hud::systems::updates::{update_enemy_text, update_score_text};
 
@@ -15,7 +16,7 @@ impl Plugin for HudPlugin {
             // OnEnter Systems TODO
             .add_systems(Startup, (spawn_hud))
             // Systems
-            .add_systems(Update, (update_score_text, update_enemy_text));
+            .add_systems(Update, (update_score_text, update_enemy_text).in_set(UpdateUI::RenderStats));
         // OnExit Systems
         // .add_system(despawn_hud.in_schedule(OnExit(AppState::Game)));
     }
